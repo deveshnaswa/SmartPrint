9cf0951e51e914b980ebdbb7a4225ac28aa46593
Devesh Naswa <devesh.naswa@gmail.com>
2017-09-04 23:58:22 +0530

    User is authenticated from the database on login form

--- SmartPrint/Controllers/AuthController.cs
+++ SmartPrint/Controllers/AuthController.cs
@@ -34,26 +34,50 @@ namespace SmartPrint.Controllers
         {
             if (!ModelState.IsValid) //Checks if input fields have the correct format
             {
-                return View(model); //Returns the view with the input values so that the user doesn't have to retype again
+                return
+                    View(model); //Returns the view with the input values so that the user doesn't have to retype again
             }
 
-            //Checks whether the input is the same as those literals. Note: Never ever do this! This is just to demo the validation while we're not yet doing any database interaction
-            if (model.UserEmail== "admin@admin.com" & model.UserPass== "123456")
+            using (var db = new MainDbContext())
             {
-                var identity = new ClaimsIdentity(new[] {
-                    new Claim(ClaimTypes.Name, "Xtian"),
-                    new Claim(ClaimTypes.Email, "xtian@email.com"),
-                    new Claim(ClaimTypes.Country, "Philippines")
-                }, "ApplicationCookie");
 
-                var ctx = Request.GetOwinContext();
-                var authManager = ctx.Authentication;
-                authManager.SignIn(identity);
+                var emailCheck = db.Users.FirstOrDefault(u => u.UserEmail== model.UserEmail);
+                var getPassword = db.Users.Where(u => u.UserEmail == model.UserEmail).Select(u => u.UserPass);
+                var materializePassword = getPassword.ToList();
+                var password = materializePassword[0];
+                var decryptedPassword = CustomDecrypt.Decrypt(password);
 
-                return RedirectToAction("Index","Home");
-            }
+                if (model.UserEmail != null && model.UserPass == decryptedPassword)
+                //Checks whether the input is the same as those literals. Note: Never ever do this! This is just to demo the validation while we're not yet doing any database interaction
+               // if (model.UserEmail == "admin@admin.com" & model.UserPass == "123456")
+                {
+                    var getName = db.Users.Where(u => u.UserEmail == model.UserEmail).Select(u =>u.FName);
+                    var materializeName = getName.ToList();
+                    var name = materializeName[0];
 
-            ModelState.AddModelError("", "Invalid email or password");
+                    //var getCountry = db.Users.Where(u = &amp; gt; u.Email == model.Email).Select(u = &amp; gt; u.Country);
+                    //var materializeCountry = getCountry.ToList();
+                    //var country = materializeCountry[0];
+
+                    var getEmail = db.Users.Where(u =>u.UserEmail== model.UserEmail).Select(u =>u.UserEmail);
+                    var materializeEmail = getEmail.ToList();
+                    var email = materializeEmail[0];
+
+                    var identity = new ClaimsIdentity(new[] {
+                        new Claim(ClaimTypes.Name, name),
+                        new Claim(ClaimTypes.Email, email)
+                    
+                    }, "ApplicationCookie");
+
+                    var ctx = Request.GetOwinContext();
+                    var authManager = ctx.Authentication;
+                    authManager.SignIn(identity);
+
+                    return RedirectToAction("Index", "Home");
+                }
+        }
+
+        ModelState.AddModelError("", "Invalid email or password");
 
 
             return View(model); //Should always be declared on the end of an action method
